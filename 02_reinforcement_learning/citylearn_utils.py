9a4a0ca8a715876074c8947204da363c3e5a517e
Johannes Exenberger <26438814+jxnb@users.noreply.github.com>
2025-06-05 06:46:08 +0200

    add lecture 2 materials

---
+++ 02_reinforcement_learning/citylearn_utils.py
@@ -0,0 +1,461 @@
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
+from citylearn.citylearn import CityLearnEnv
+
+
+def get_kpis(env: CityLearnEnv) -> pd.DataFrame:
+    """Returns evaluation KPIs.
+
+    Electricity cost and carbon emissions KPIs are provided
+    at the building-level and average district-level. Average daily peak,
+    ramping and (1 - load factor) KPIs are provided at the district level.
+
+    Parameters
+    ----------
+    env: CityLearnEnv
+        CityLearn environment instance.
+
+    Returns
+    -------
+    kpis: pd.DataFrame
+        KPI table.
+    """
+
+    kpis = env.unwrapped.evaluate()
+
+    # names of KPIs to retrieve from evaluate function
+    kpi_names = {
+        'cost_total': 'Cost',
+        'carbon_emissions_total': 'Emissions',
+        'daily_peak_average': 'Avg. daily peak',
+        'ramping_average': 'Ramping',
+        'monthly_one_minus_load_factor_average': '1 - load factor'
+    }
+    kpis = kpis[
+        (kpis['cost_function'].isin(kpi_names))
+    ].dropna()
+    kpis['cost_function'] = kpis['cost_function'].map(lambda x: kpi_names[x])
+
+    # round up the values to 2 decimal places for readability
+    kpis['value'] = kpis['value'].round(2)
+
+    # rename the column that defines the KPIs
+    kpis = kpis.rename(columns={'cost_function': 'kpi'})
+
+    return kpis
+
+
+def plot_building_kpis(envs: dict[str, CityLearnEnv]) -> plt.Figure:
+    """Plots electricity consumption, cost and carbon emissions
+    at the building-level for different control agents in bar charts.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure containing plotted axes.
+    """
+
+    kpis_list = []
+
+    for k, v in envs.items():
+        kpis = get_kpis(v)
+        kpis = kpis[kpis['level']=='building'].copy()
+        kpis['building_id'] = kpis['name'].str.split('_', expand=True)[1]
+        kpis['building_id'] = kpis['building_id'].astype(int).astype(str)
+        kpis['env_id'] = k
+        kpis_list.append(kpis)
+
+    kpis = pd.concat(kpis_list, ignore_index=True, sort=False)
+    kpi_names= kpis['kpi'].unique()
+    column_count_limit = 3
+    row_count = math.ceil(len(kpi_names)/column_count_limit)
+    column_count = min(column_count_limit, len(kpi_names))
+    building_count = len(kpis['name'].unique())
+    env_count = len(envs)
+    figsize = (3.0*column_count, 0.3*env_count*building_count*row_count)
+    fig, _ = plt.subplots(
+        row_count, column_count, figsize=figsize, sharey=True
+    )
+
+    for i, (ax, (k, k_data)) in enumerate(zip(fig.axes, kpis.groupby('kpi'))):
+        sns.barplot(x='value', y='name', data=k_data, hue='env_id', ax=ax)
+        ax.set_xlabel(None)
+        ax.set_ylabel(None)
+        ax.set_title(k)
+
+        for j, _ in enumerate(envs):
+            ax.bar_label(ax.containers[j], fmt='%.2f')
+
+        if i == len(kpi_names) - 1:
+            ax.legend(
+                loc='upper left', bbox_to_anchor=(1.3, 1.0), framealpha=0.0
+            )
+        else:
+            ax.legend().set_visible(False)
+
+        for s in ['right','top']:
+            ax.spines[s].set_visible(False)
+
+    return fig
+
+
+def plot_district_kpis(envs: dict[str, CityLearnEnv]) -> plt.Figure:
+    """Plots electricity consumption, cost, carbon emissions,
+    average daily peak, ramping and (1 - load factor) at the
+    district-level for different control agents in a bar chart.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure containing plotted axes.
+    """
+
+    kpis_list = []
+
+    for k, v in envs.items():
+        kpis = get_kpis(v)
+        kpis = kpis[kpis['level']=='district'].copy()
+        kpis['env_id'] = k
+        kpis_list.append(kpis)
+
+    kpis = pd.concat(kpis_list, ignore_index=True, sort=False)
+    row_count = 1
+    column_count = 1
+    env_count = len(envs)
+    kpi_count = len(kpis['kpi'].unique())
+    figsize = (6.0*column_count, 0.225*env_count*kpi_count*row_count)
+    fig, ax = plt.subplots(row_count, column_count, figsize=figsize)
+    sns.barplot(x='value', y='kpi', data=kpis, hue='env_id', ax=ax)
+    ax.set_xlabel(None)
+    ax.set_ylabel(None)
+
+    for j, _ in enumerate(envs):
+        ax.bar_label(ax.containers[j], fmt='%.2f')
+
+    for s in ['right','top']:
+        ax.spines[s].set_visible(False)
+
+    ax.legend(loc='upper left', bbox_to_anchor=(1.3, 1.0), framealpha=0.0)
+    plt.tight_layout()
+
+    return fig
+
+
+def plot_building_load_profiles(
+    envs: dict[str, CityLearnEnv], daily_average: bool = None
+) -> plt.Figure:
+    """Plots building-level net electricty consumption profile
+    for different control agents.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+    daily_average: bool, default: False
+        Whether to plot the daily average load profile.
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure containing plotted axes.
+    """
+
+    daily_average = False if daily_average is None else daily_average
+    building_count = len(list(envs.values())[0].buildings)
+    column_count_limit = 4
+    row_count = math.ceil(building_count/column_count_limit)
+    column_count = min(column_count_limit, building_count)
+    figsize = (4.0*column_count, 1.75*row_count)
+    fig, _ = plt.subplots(row_count, column_count, figsize=figsize)
+
+    for i, ax in enumerate(fig.axes):
+        for k, v in envs.items():
+            y = v.unwrapped.buildings[i].net_electricity_consumption
+            y = np.reshape(y, (-1, 24)).mean(axis=0) if daily_average else y
+            x = range(len(y))
+            ax.plot(x, y, label=k)
+
+        ax.set_title(v.unwrapped.buildings[i].name)
+        ax.set_ylabel('kWh')
+
+        if daily_average:
+            ax.set_xlabel('Hour')
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+
+        else:
+            ax.set_xlabel('Time step')
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(24))
+
+        if i == building_count - 1:
+            ax.legend(
+                loc='upper left', bbox_to_anchor=(1.0, 1.0), framealpha=0.0
+            )
+        else:
+            ax.legend().set_visible(False)
+
+
+    plt.tight_layout()
+
+    return fig
+
+
+def plot_district_load_profiles(
+    envs: dict[str, CityLearnEnv], daily_average: bool = None
+) -> plt.Figure:
+    """Plots district-level net electricty consumption profile
+    for different control agents.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+    daily_average: bool, default: False
+        Whether to plot the daily average load profile.
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure containing plotted axes.
+    """
+
+    daily_average = False if daily_average is None else daily_average
+    figsize = (5.0, 1.5)
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    for k, v in envs.items():
+        y = v.unwrapped.net_electricity_consumption
+        y = np.reshape(y, (-1, 24)).mean(axis=0) if daily_average else y
+        x = range(len(y))
+        ax.plot(x, y, label=k)
+
+    ax.set_ylabel('kWh')
+
+    if daily_average:
+        ax.set_xlabel('Hour')
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+
+    else:
+        ax.set_xlabel('Time step')
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(24))
+
+    ax.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), framealpha=0.0)
+
+    plt.tight_layout()
+    return fig
+
+
+def plot_battery_soc_profiles(envs: dict[str, CityLearnEnv]) -> plt.Figure:
+    """Plots building-level battery SoC profiles fro different control agents.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure containing plotted axes.
+    """
+
+    building_count = len(list(envs.values())[0].buildings)
+    column_count_limit = 4
+    row_count = math.ceil(building_count/column_count_limit)
+    column_count = min(column_count_limit, building_count)
+    figsize = (4.0*column_count, 1.75*row_count)
+    fig, _ = plt.subplots(row_count, column_count, figsize=figsize)
+
+    for i, ax in enumerate(fig.axes):
+        for k, v in envs.items():
+            y = np.array(v.unwrapped.buildings[i].electrical_storage.soc)
+            x = range(len(y))
+            ax.plot(x, y, label=k)
+
+        ax.set_title(v.unwrapped.buildings[i].name)
+        ax.set_xlabel('Time step')
+        ax.set_ylabel('SoC')
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(24))
+        ax.set_ylim(0.0, 1.0)
+
+        if i == building_count - 1:
+            ax.legend(
+                loc='upper left', bbox_to_anchor=(1.0, 1.0), framealpha=0.0
+            )
+        else:
+            ax.legend().set_visible(False)
+
+
+    plt.tight_layout()
+
+    return fig
+
+
+def plot_simulation_summary(envs: dict[str, CityLearnEnv]):
+    """Plots KPIs, load and battery SoC profiles for different control agents.
+
+    Parameters
+    ----------
+    envs: dict[str, CityLearnEnv]
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+    """
+
+    print('#'*8 + ' BUILDING-LEVEL ' + '#'*8)
+    print('Building-level KPIs:')
+    _ = plot_building_kpis(envs)
+    plt.show()
+
+    print('Building-level simulation period load profiles:')
+    _ = plot_building_load_profiles(envs)
+    plt.show()
+
+    print('Building-level daily-average load profiles:')
+    _ = plot_building_load_profiles(envs, daily_average=True)
+    plt.show()
+
+    print('Battery SoC profiles:')
+    _ = plot_battery_soc_profiles(envs)
+    plt.show()
+
+    print('#'*8 + ' DISTRICT-LEVEL ' + '#'*8)
+    print('District-level KPIs:')
+    _ = plot_district_kpis(envs)
+    plt.show()
+
+    print('District-level simulation period load profiles:')
+    _ = plot_district_load_profiles(envs)
+    plt.show()
+
+    print('District-level daily-average load profiles:')
+    _ = plot_district_load_profiles(envs, daily_average=True)
+    plt.show()
+
+
+def plot_table(
+    fig, ax: plt.Axes, table: np.ndarray, title: str, cmap: str,
+    colorbar_label: str, xlabel: str, ylabel: str
+) -> plt.Axes:
+    """Plot 2-dimensional table on a heat map.
+
+    Parameters
+    ----------
+    ax: plt.Axes
+        Figure axes
+    table: np.ndarray
+        Table array
+    title: str
+        axes title
+    cmap: str
+        Colormap
+    colorbar_label: str
+        Colorbar name
+    xlabel: str
+        Heat map x-axis label
+    ylabel: str
+        Heat map y-axis label
+
+    Returns
+    -------
+    ax: plt.Axes
+        Plotted axes
+    """
+
+    x = list(range(table.shape[0]))
+    y = list(range(table.shape[1]))
+    z = table.T
+    pcm = ax.pcolormesh(
+        x, y, z, shading='nearest', cmap=cmap,
+        edgecolors='black', linewidth=0.0
+    )
+    _ = fig.colorbar(
+        pcm, ax=ax, orientation='horizontal',
+        label=colorbar_label, fraction=0.025, pad=0.08
+    )
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    return ax
+
+
+def plot_exploration_exploitation(tql_model, tql_episodes):
+    fig, ax = plt.subplots(1, 1, figsize=(4, 2))
+    y = np.array([max(
+        tql_model.minimum_epsilon,
+        tql_model.epsilon_init*np.exp(-tql_model.epsilon_decay*e)
+    ) for e in range(100_000)])
+    ref_x = len(y) - len(y[y <= 0.5]) - 1
+    ref_y = y[ref_x]
+    ax.plot(y)
+    ax.axvline(ref_x, color='red', linestyle=':')
+    ax.axhline(ref_y, color='red', linestyle=':')
+    ax.axvline(tql_episodes, color='green', linestyle=':')
+    ax.set_xlabel('Episode')
+    text = f'{ref_x} training episodes needed to get\nat least 50%'\
+        ' exploitation probability.'
+    ax.text(ref_x + 1000, ref_y + 0.05, text, color='red')
+    ax.text(
+        tql_episodes + 1000,
+        ref_y - 0.1,
+        f'Current training episodes = {tql_episodes}',
+        va='bottom', color='green'
+    )
+    ax.set_ylabel(r'$\epsilon$')
+    plt.show()
+
+
+def plot_actions(actions_list: list[list[float]], building_names: list[str], title: str) -> plt.Figure:
+    """Plots action time series for different buildings
+
+    Parameters
+    ----------
+    actions_list: list[list[float]]
+        List of actions where each element with index, i,
+        in list is a list of the actions for different buildings
+        taken at time step i.
+    building_names: list[str]:
+        List of build names that map to the action lists.
+    title: str
+        Plot axes title
+
+    Returns
+    -------
+    fig: plt.Figure
+        Figure with plotted axes
+
+    """
+
+    fig, ax = plt.subplots(1, 1, figsize=(6, 1))
+    plot_data = pd.DataFrame(actions_list, columns=building_names)
+    x = list(range(plot_data.shape[0]))
+
+    for c in plot_data.columns:
+        y = plot_data[c].tolist()
+        ax.plot(x, y, label=c)
+
+    ax.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), framealpha=0.0)
+    ax.set_xlabel('Time step')
+    ax.set_ylabel(r'$\frac{kWh}{kWh_{capacity}}$')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(24))
+    ax.set_title(title)
+
+    return fig
